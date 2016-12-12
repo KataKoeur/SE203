7459df35c6b20e0c3e8365f289980410003ab26a
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-12-12 18:14:22 +0100

    exo2 terminé

--- exos_c/exo2/exo2.c
+++ exos_c/exo2/exo2.c
@@ -1,21 +1,31 @@
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 
-int32_t x =34;
-int32_t y;
-
-const char mesg[] ="Hello World!\n";
+//variable globale initialisé => .data
+int32_t globale1 =34;
+int32_t globale2 =34;
 
 int main()
 {
-  static uint8_t z;
-  uint16_t t;
+  //variable pointeur => .heap
+  uint16_t *ptr1 = (uint16_t*) malloc(16);
+  uint16_t *ptr2 = (uint16_t*) malloc(16);
 
-  y = 12;
-  z = z + 1;
-  t = y + z;
+  //variable locale non static initialisé => .stack
+  uint16_t locale1 = 12;
+  uint16_t locale2 = 12;
+  uint16_t locale3 = 12;
+  uint16_t locale4 = 12;
 
-  printf(mesg);
-  printf("x = %d, y = %d, z = %d, t = %d\n", x, y, z, t);
+  printf("globale1  = %p\n", &globale1);
+  printf("globale2  = %p\n", &globale2);
+  printf("ptr1      = %p\n", ptr1);
+  printf("ptr2      = %p\n", ptr2);
+  printf("locale1   = %p\n", &locale1);
+  printf("locale2   = %p\n", &locale2);
+  printf("locale3   = %p\n", &locale3);
+  printf("locale4   = %p\n", &locale4);
+
   return 0;
 }
