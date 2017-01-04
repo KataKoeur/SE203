8d8d033fc839ad527df5355d6012aa8b067f6478
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-01-04 18:45:06 +0100

    Suite de Fibonacci

--- bare-metal/main.c
+++ bare-metal/main.c
@@ -1,4 +1,16 @@
+int fibo(int n)
+{
+   if(n<1)                 return 0;
+   else if(n==1 || n==2)   return 1;
+   else                    return fibo(n-1) + fibo(n-2);
+}
+
 int main (void)
 {
-   while(1);
+   int result = 0;
+
+   while(1)
+   {
+      result = fibo(8);
+   }
 }
