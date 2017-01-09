dc61858e7a3d2a5f6aa30856dd770df1a2deb69a
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-01-09 11:15:41 +0100

    Debug de init et ld_ram

--- bare-metal/init.c
+++ bare-metal/init.c
@@ -1,10 +1,9 @@
 void init_bss()
 {
-   extern char _end_of_text;   
    extern char _start_of_bss;   
    extern char _end_of_bss;   
 
-   int *dst = &_end_of_text;
+   char *dst;
 
    for(dst = &_start_of_bss; dst < &_end_of_bss; dst++)
    {
