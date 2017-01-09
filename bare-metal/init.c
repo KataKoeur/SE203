ceb46fd8dd1c548a00a5ae48f5a228903ddcc014
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-01-09 10:51:13 +0100

    Update init.c, work in progress

--- bare-metal/init.c
+++ bare-metal/init.c
@@ -1,5 +1,9 @@
 void init_bss()
 {
+   extern char _end_of_text;   
+   extern char _start_of_bss;   
+   extern char _end_of_bss;   
+
    int *dst = &_end_of_text;
 
    for(dst = &_start_of_bss; dst < &_end_of_bss; dst++)
