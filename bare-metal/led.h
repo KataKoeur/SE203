81dcbc4d44e8e084ed4f4d1b2805400dd65c4d8c
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-01-11 14:43:05 +0100

    Ajout d'une garde dans led.h

--- bare-metal/led.h
+++ bare-metal/led.h
@@ -1,5 +1,8 @@
 //led.h
 
+#ifndef LED_H
+#define LED_H
+
 #define SIM_SCGC5   (*(volatile int *)0x40048038)
 #define PORTD_PCR5  (*(volatile int *)0x4004c014)
 #define PORTE_PCR29 (*(volatile int *)0x4004d074)
@@ -9,3 +12,5 @@
 #define GPIOE_PCOR  (*(volatile int *)0x400ff108)
 
 void led_init();
+
+#endif
