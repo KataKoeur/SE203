e84c71a7060142ea86c7aaa01855df59418148f9
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-01-11 15:14:56 +0100

    Allumage des led fonctionnelles

--- bare-metal/led.c
+++ bare-metal/led.c
@@ -5,19 +5,19 @@
 void led_init()
 {
    //activation horloge port D et E
-   SIM_SCGC5 = SIM_SCGC5 | 0b11000000000000;
+   SIM_SCGC5 = SIM_SCGC5 | 0x00003000;
 
    //config GPIO (MUX = 001)
-   PORTD_PCR5  = PORTD_PCR5  |  0b00100000000;
-   PORTD_PCR5  = PORTD_PCR5  & ~0b11000000000;
-   PORTE_PCR29 = PORTE_PCR29 |  0b00100000000;
-   PORTE_PCR29 = PORTE_PCR29 & ~0b11000000000;
+   PORTD_PCR5  = PORTD_PCR5  |  0x00000100;
+   PORTD_PCR5  = PORTD_PCR5  & ~0x00000600;
+   PORTE_PCR29 = PORTE_PCR29 |  0x00000100;
+   PORTE_PCR29 = PORTE_PCR29 & ~0x00000600;
 
    //config sortie
-   GPIOD_PDDR = 1;
-   GPIOE_PDDR = 1;
+   GPIOD_PDDR = 0xffffffff;
+   GPIOE_PDDR = 0xffffffff;
 
    //allumage des leds
-   GPIOD_PCOR = 0;
-   GPIOE_PCOR = 0;
+   GPIOD_PCOR = 0x00000000;
+   GPIOE_PCOR = 0x00000000;
 }
