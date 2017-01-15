50c61d4a1b1c1cfb78bf5639af71e1eba6421513
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-01-16 00:55:18 +0100

    Activation horloge port A

--- bare-metal/uart.c
+++ bare-metal/uart.c
@@ -21,4 +21,8 @@ void uart_init()
    UART0_C1  = UART0_C1  & ~0x10; //8bits de données
    UART0_C1  = UART0_C1  & ~0x02; //pas de parité
    UART0_BDH = UART0_BDH & ~0x20; //1bit de stop
+
+   //broche RX et TX du portA en mode UART
+   SIM_SCGC5 = SIMSCGC5 | 0x00000200;
+   
 }
--- bare-metal/uart.h
+++ bare-metal/uart.h
@@ -5,6 +5,7 @@
 
 #define SIM_SOPT2 (*(volatile int *)0x40048004)
 #define SIM_SCGC4 (*(volatile int *)0x40048034)
+#define SIM_SCGC5 (*(volatile int *)0x40048038)
 #define UART0_C1  (*(volatile int *)0x4006a002)
 #define UART0_C4  (*(volatile int *)0x4006a00a)
 #define UART0_BDH (*(volatile int *)0x4006a000)
