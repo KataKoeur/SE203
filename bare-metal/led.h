f1a94e46554483927ea8660309423e88f509e1a4
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-01-16 14:11:08 +0100

    Update header avec uint32_t

--- bare-metal/led.h
+++ bare-metal/led.h
@@ -3,17 +3,19 @@
 #ifndef LED_H
 #define LED_H
 
-#define SIM_SCGC5   (*(volatile int *)0x40048038)
-#define PORTD_PCR5  (*(volatile int *)0x4004c014)
-#define PORTE_PCR29 (*(volatile int *)0x4004d074)
-#define GPIOD_PDDR  (*(volatile int *)0x400ff0d4)
-#define GPIOE_PDDR  (*(volatile int *)0x400ff114)
-#define GPIOD_PCOR  (*(volatile int *)0x400ff0c8)
-#define GPIOE_PCOR  (*(volatile int *)0x400ff108)
-#define GPIOD_PSOR  (*(volatile int *)0x400ff0c4)
-#define GPIOE_PSOR  (*(volatile int *)0x400ff104)
-#define GPIOD_PTOR  (*(volatile int *)0x400ff0cc)
-#define GPIOE_PTOR  (*(volatile int *)0x400ff10c)
+#include <stdint.h>
+
+#define SIM_SCGC5   (*(volatile uint32_t *)0x40048038)
+#define PORTD_PCR5  (*(volatile uint32_t *)0x4004c014)
+#define PORTE_PCR29 (*(volatile uint32_t *)0x4004d074)
+#define GPIOD_PDDR  (*(volatile uint32_t *)0x400ff0d4)
+#define GPIOE_PDDR  (*(volatile uint32_t *)0x400ff114)
+#define GPIOD_PCOR  (*(volatile uint32_t *)0x400ff0c8)
+#define GPIOE_PCOR  (*(volatile uint32_t *)0x400ff108)
+#define GPIOD_PSOR  (*(volatile uint32_t *)0x400ff0c4)
+#define GPIOE_PSOR  (*(volatile uint32_t *)0x400ff104)
+#define GPIOD_PTOR  (*(volatile uint32_t *)0x400ff0cc)
+#define GPIOE_PTOR  (*(volatile uint32_t *)0x400ff10c)
 
 void led_init();
 void led_g_on();
