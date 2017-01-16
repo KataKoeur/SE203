7d8b4924a409c4545c477593ee651ff5240ea543
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-01-16 11:48:47 +0100

    Update uart.c

--- bare-metal/uart.c
+++ bare-metal/uart.c
@@ -5,7 +5,7 @@
 void uart_init()
 {
    //activation horologe UART0
-   SIM_SOPT2 = SIM_SOPT2 |  0x04000000;
+   SIM_SOPT2 = SIM_SOPT2 |  0x04010000;
    SIM_SOPT2 = SIM_SOPT2 & ~0x08000000;
    SIM_SCGC4 = SIM_SCGC4 |  0x00000400;
 
