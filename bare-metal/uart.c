a6e31b527630a526a9d9e59ddd17b9380d4e7d7b
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-01-16 01:22:02 +0100

    Broche Tx et Rx du port A en mode UART

--- bare-metal/uart.c
+++ bare-metal/uart.c
@@ -24,5 +24,10 @@ void uart_init()
 
    //broche RX et TX du portA en mode UART
    SIM_SCGC5 = SIMSCGC5 | 0x00000200;
-   
+   //RX
+   PORTA_PCR1  = PORTA_PCR1  |  0x00000300;
+   PORTA_PCR1  = PORTA_PCR1  & ~0x00000400;
+   //TX 
+   PORTA_PCR2  = PORTA_PCR2  |  0x00000300;
+   PORTA_PCR2  = PORTA_PCR2  & ~0x00000400;
 }
--- bare-metal/uart.h
+++ bare-metal/uart.h
@@ -3,13 +3,15 @@
 #ifndef UART_H
 #define UART_H
 
-#define SIM_SOPT2 (*(volatile int *)0x40048004)
-#define SIM_SCGC4 (*(volatile int *)0x40048034)
-#define SIM_SCGC5 (*(volatile int *)0x40048038)
-#define UART0_C1  (*(volatile int *)0x4006a002)
-#define UART0_C4  (*(volatile int *)0x4006a00a)
-#define UART0_BDH (*(volatile int *)0x4006a000)
-#define UART0_BDL (*(volatile int *)0x4006a001)
+#define SIM_SOPT2  (*(volatile int *)0x40048004)
+#define SIM_SCGC4  (*(volatile int *)0x40048034)
+#define SIM_SCGC5  (*(volatile int *)0x40048038)
+#define UART0_C1   (*(volatile int *)0x4006a002)
+#define UART0_C4   (*(volatile int *)0x4006a00a)
+#define UART0_BDH  (*(volatile int *)0x4006a000)
+#define UART0_BDL  (*(volatile int *)0x4006a001)
+#define PORTA_PCR1 (*(volatile int *)0x40049004)
+#define PORTA_PCR2 (*(volatile int *)0x40049008)
 
 void uart_init();
 
