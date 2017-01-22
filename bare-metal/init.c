b1f54abdce03ab47ba9e50d410c33cb1d3be5f59
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-01-22 21:17:29 +0100

    Update init.c

--- bare-metal/init.c
+++ bare-metal/init.c
@@ -1,11 +1,11 @@
 //init.c
 
+extern char _start_of_bss;
+extern char _end_of_bss;
+
 void init_bss()
 {
-   extern char _start_of_bss;   
-   extern char _end_of_bss;   
-
-   char *dst = &_start_of_bss;
+   char *dst;
 
    for(dst = &_start_of_bss; dst < &_end_of_bss; dst++)
    {
