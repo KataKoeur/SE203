5c5b4d5dfb1538b5f25df90b71cef5774a22a9b5
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-01-25 11:47:52 +0100

    Update table des vecteurs - wip

--- bare-metal/irq.c
+++ bare-metal/irq.c
@@ -2,28 +2,58 @@
 
 void *vector_table[] = 
 {
-    // Stack and Reset Handler
-    &_stack,            /* Top of stack */
-    _start,             /* Reset handler */
+   // Stack and Reset Handler
+   &_stack,            /* Top of stack */
+   _start,             /* Reset handler */
 
-    // ARM internal exceptions
-    NMI_Handler,        /* NMI handler */
-    HardFault_Handler,  /* Hard Fault handler */
-    0,                  /* Reserved */
-    0,                  /* Reserved */
-    0,                  /* Reserved */
-    0,                  /* Reserved */
-    0,                  /* Reserved */
-    0,                  /* Reserved */
-    0,                  /* Reserved */
-    SVC_Handler,        /* SVC handler */
-    0,                  /* Reserved */
-    0,                  /* Reserved */
-    PendSV_Handler,     /* Pending SVC handler */
-    SysTick_Handler,    /* SysTick hanlder */
+   // ARM internal exceptions
+   NMI_Handler,        /* NMI handler */
+   HardFault_Handler,  /* Hard Fault handler */
+   0,                  /* Reserved */
+   0,                  /* Reserved */
+   0,                  /* Reserved */
+   0,                  /* Reserved */
+   0,                  /* Reserved */
+   0,                  /* Reserved */
+   0,                  /* Reserved */
+   SVC_Handler,        /* SVC handler */
+   0,                  /* Reserved */
+   0,                  /* Reserved */
+   PendSV_Handler,     /* Pending SVC handler */
+   SysTick_Handler,    /* SysTick hanlder */
 
-    // KL46 External interrupts
-    DMA0_IRQHandler,         /* DMA0 interrupt */
-    DMA1_IRQHandler,         /* DMA1 interrupt */
-    DMA2_IRQHandler,         /* DMA2 interrupt */
+   // KL46 External interrupts
+   DMA0_IRQHandler,    /* DMA0 interrupt */
+   DMA1_IRQHandler,    /* DMA1 interrupt */
+   DMA2_IRQHandler,    /* DMA2 interrupt */
+   DMA3_IRQHandler,    /* DMA3 interrupt */
+   0,                  /* Reserved */
+   FTFA_IRQHandler,    /* Commande complete and read collision */
+   PMC_IRQHandler,     /* Low-voltage detect, low-voltage warning */
+   LLWU_IRQHandler,    /* Low Leakage Wakeup */
+   I2C0_IRQHandler,    /* - */
+   I2C1_IRQHandler,    /* - */
+   0,                  /* Reserved */
+   SPI0_IRQHandler,    /* Single interrupt vector for all sources */
+   SPI1_IRQHandler,    /* Single interrupt vector for all sources */
+   UART0_IRQHandler,   /* Status and error */
+   UART1_IRQHandler,   /* Status and error */
+   UART2_IRQHandler,   /* Status and error */
+   ADC0_IRQHandler,    /* - */
+   CMP0_IRQHandler,    /* - */
+   TPM0_IRQHandler,    /* - */
+   TPM1_IRQHandler,    /* - */
+   TPM2_IRQHandler,    /* - */
+   RTC0_IRQHandler,    /* Alarm interrupt */
+   RTC1_IRQHandler,    /* Seconds interrupt */
+   PIT_IRQHandler,     /* Single interrupt vector for all channels */
+   I2S0_IRQHandler,    /* Single interrupt vector for all sources */
+   USB OTG_IRQHandler, /* - */
+   DAC0_IRQHandler,    /* - */
+   TSI0_IRQHandler,    /* - */
+   MCG_IRQHandler,     /* - */
+   LPTMR0_IRQHandler,  /* - */
+   SLCD_IRQHandler,    /* - */
+   PORTA_IRQHandler,   /* Pin detect (Port A) */
+   PORTCD_IRQHandler,  /* Pin detect (Single interrupt vector for Port C and Port D) */
 };
