ae149004e351c9716749f59349fa16b84ccb0acb
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-01-25 18:30:42 +0100

    Ajout fonction button_init()

--- bare-metal/buttons.c
+++ bare-metal/buttons.c
@@ -1,6 +1,22 @@
 //buttons.c
 
+#include "irq.h"
+
+#define SIM_SCGC5  (*(volatile uint32_t *)0x40048038)
+#define PORTC_PCR3 (*(volatile uint32_t *)0x4004b00c)
+#define GPIOC_PDDR (*(volatile uint32_t *)0x400ff094)
+
 void button_init(void)
 {
+   //activation horloge port C
+   SIM_SCGC5 |= 0x00000800;
 
+   //config du port C3
+   PORTC_PCR3 = (PORTC_PCR3 | 0x000a0103) & ~0x00050600;
+
+   //config entrée
+   GPIOC_PDDR &= ~0x00000008;
+
+   //activation interrupttion port C
+   irq_enable(31);
 }
