f5540c58e9c5733a9b5f973ed4e2ed01bbdf5381
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-01-27 18:27:55 +0100

    Ajout fonction pit_init()

--- bare-metal/pit.c
+++ bare-metal/pit.c
@@ -2,7 +2,22 @@
 
 #include "pit.h"
 
+#define PIT_MCR    (*(volatile uint32_t *)0x40037000)
+#define PIT_LDVAL0 (*(volatile uint32_t *)0x40037100)
+#define PIT_TCTRL0 (*(volatile uint32_t *)0x40037108)
+#define PIT_TFLG0 (*(volatile uint32_t *)0x4003710c)
+
 void pit_init()
 {
+   //activation horloge PIT
+   PIT_MCR = 0x00000000;
 
+   //fréquence des interruptions (1s)
+   PIT_LDVAL0 = 0x016e3600;
+
+   //abaissement du flag
+   PIT_TFLG0 = 0x00000001;
+
+   //activation timer et interruption
+   PIT_TCTRL0 = 0x00000003;
 }
