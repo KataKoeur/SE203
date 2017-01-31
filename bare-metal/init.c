fdf957455f6fd02a87ffe5607a0645f5198c1966
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-01-31 21:08:29 +0100

    recopie flash vers RAM - echec

--- bare-metal/init.c
+++ bare-metal/init.c
@@ -15,13 +15,28 @@ void init_bss()
 
 void init_memory()
 {
+   extern char _start_of_flash_vectors;
+   extern char _end_of_flash_vectors;
+
+   extern char _start_of_text;
    extern char _end_of_text;
+
    extern char _start_of_data;
    extern char _end_of_data;
 
-   char *src = &_end_of_text;
-   char *dst = &_start_of_data;
-   
-   while (dst < &_end_of_data) 
-      *dst++ = *src++;
+   extern char _end_of_bss;   
+
+   char *src = &_start_of_flash_vectors;
+   char *dst = &_end_of_bss;
+
+   //flash_vectors vers RAM
+   while (src < &_end_of_flash_vectors) *dst++ = *src++;
+
+   //text vers RAM
+   src = &_start_of_text;
+   while (src < &_end_of_text) *dst++ = *src++;
+
+   //data vers RAM
+   src = &_start_of_data;
+   while (src < &_end_of_data) *dst++ = *src++;
 }
