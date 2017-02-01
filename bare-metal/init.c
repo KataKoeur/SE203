7a0984d6c5ee5c5acf6d2a099a39243d202a7477
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-02-01 16:08:55 +0100

    Update recopie en RAM - wip

--- bare-metal/init.c
+++ bare-metal/init.c
@@ -15,28 +15,30 @@ void init_bss()
 
 void init_memory()
 {
-   extern char _start_of_flash_vectors;
-   extern char _end_of_flash_vectors;
+   extern char _start_of_flash_vectors_LMA;
+   extern char _start_of_flash_vectors_VMA;
+   extern char _end_of_flash_vectors_VMA;
 
-   extern char _start_of_text;
-   extern char _end_of_text;
+   extern char _start_of_text_LMA;
+   extern char _start_of_text_VMA;
+   extern char _end_of_text_VMA;
 
-   extern char _start_of_data;
-   extern char _end_of_data;
-
-   extern char _end_of_bss;   
-
-   char *src = &_start_of_flash_vectors;
-   char *dst = &_end_of_bss;
+   extern char _start_of_data_LMA;
+   extern char _start_of_data_VMA;
+   extern char _end_of_data_VMA;
 
    //flash_vectors vers RAM
-   while (src < &_end_of_flash_vectors) *dst++ = *src++;
+   char *src = &_start_of_flash_vectors_LMA;
+   char *dst = &_start_of_flash_vectors_VMA;
+   while (dst < &_end_of_flash_vectors_VMA) *dst++ = *src++;
 
    //text vers RAM
-   src = &_start_of_text;
-   while (src < &_end_of_text) *dst++ = *src++;
+   src = &_start_of_text_LMA;
+   dst = &_start_of_text_VMA;
+   while (dst < &_end_of_text_VMA) *dst++ = *src++;
 
    //data vers RAM
-   src = &_start_of_data;
-   while (src < &_end_of_data) *dst++ = *src++;
+   src = &_start_of_data_LMA;
+   dst = &_start_of_data_VMA;
+   while (dst < &_end_of_data_VMA) *dst++ = *src++;
 }
